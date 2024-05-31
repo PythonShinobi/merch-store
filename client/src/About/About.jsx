87ba99496d0fdfe8eec2cf4084da84d5f7137525
PythonShinobi <pythonshinobi@gmail.com>
2024-05-31 11:40:41 +0300

    Add the AboutUs component.

---
+++ client/src/About/About.jsx
@@ -0,0 +1,20 @@
+// client/src/About/About.jsx
+import React from "react";
+
+import "./About.css";
+
+const AboutUs = () => {
+  return (
+    <section id="about">
+      <h1 className="heading"><span>About</span> us</h1>
+      <p>
+        Welcome to our anime merchandise store, where passion meets fashion! At our store, we take pride in offering high-quality anime apparel and accessories in a wide range of sizes.
+        From trendy apparel to timeless pieces, we curate our collection with care to ensure that every anime enthusiast finds something they love.
+        Our merchandise is crafted from premium materials, ensuring both comfort and durability. Whether you're looking for the latest trends or classic favorites,
+        we've got you covered with a diverse selection that caters to every anime fan's style.
+      </p>
+    </section>
+  );
+};
+
+export default AboutUs;

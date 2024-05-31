144b41f7ef61b0f95d28837bcac61837d901b33a
PythonShinobi <pythonshinobi@gmail.com>
2024-05-31 10:14:27 +0300

    Add hero section to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./Navbar/Navbar";
+import Hero from "./Hero/Hero";
 
 function App() {
     return (
         <Router>            
-            <Navbar />            
+            <Navbar />        
+            <Hero />   
         </Router>
     );
 };

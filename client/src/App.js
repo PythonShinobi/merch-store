7dd3b82dfdc50cdef63770c318c4b7488d01da2b
PythonShinobi <pythonshinobi@gmail.com>
2024-05-31 11:12:19 +0300

    Add T-shirt and Hoodie component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
+import TShirts from "./Merch/T-shirts";
+import Hoodies from "./Merch/Hoodies";
 
 function App() {
     return (
         <Router>            
             <Navbar />        
             <Hero />   
+            <TShirts />
+            <Hoodies />
         </Router>
     );
 };

1d7bd6b724c1781e849c944009272c5339f673af
PythonShinobi <pythonshinobi@gmail.com>
2024-06-03 11:07:09 +0300

    Update navbar to navigate to different sections on the website.

--- client/src/Navbar/Navbar.jsx
+++ client/src/Navbar/Navbar.jsx
@@ -1,14 +1,13 @@
 // client/src/Navbar/Navbar.jsx
 import React, { useState, useEffect, useRef } from "react";
-import { Link, useLocation } from "react-router-dom";
 import { IoClose, IoMenu} from "react-icons/io5";
 
 import "./Navbar.css";
 
-const Navbar = () => {
-  const location = useLocation();
-  const [showMenu, setShowMenu] = useState(false);
+const Navbar = () => {  
   const navRef = useRef(null);
+  const [showMenu, setShowMenu] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
 
   // Function to toggle the visibility of the menu.
   const toggleMenu = () => {
@@ -22,6 +21,42 @@ const Navbar = () => {
       setShowMenu(false);
     }
   };
+
+  useEffect(() => {
+    // Select all sections in the document
+    const sections = document.querySelectorAll("section");
+  
+    // Define options for the IntersectionObserver
+    const options = {
+      root: null, // Use the viewport as the container
+      rootMargin: "0px", // No margin around the root
+      threshold: 0.6, // Trigger callback when 60% of the section is visible
+    };
+  
+    // Create a new IntersectionObserver instance
+    const observer = new IntersectionObserver((entries) => {
+      // Iterate over each entry (section) being observed
+      entries.forEach((entry) => {
+        // Check if the section is currently intersecting (visible)
+        if (entry.isIntersecting) {
+          // Update the active section state with the current section's ID
+          setActiveSection(entry.target.id);
+        }
+      });
+    }, options);
+  
+    // Observe each section
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  
+    // Cleanup function to unobserve sections when component unmounts
+    return () => {
+      sections.forEach((section) => {
+        observer.unobserve(section);
+      });
+    };
+  }, []);  
 
   // An event listener to detect clicks outside of the navigation menu.
   useEffect(() => {
@@ -43,33 +78,33 @@ const Navbar = () => {
     <header>
       <div className="logo">My Fashion</div>
       <nav ref={navRef}>
-      <div className={`nav-menu ${showMenu ? "show-menu" : ""}`} id="nav-menu">
-        <ul>
-          <li>
-            <Link to="/" className={`nav-link ${location.pathname === "/" ? "active-link" : ""}`} onClick={closeMenuOnMobile}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/collection" className={`nav-link ${location.pathname === "/collection" ? "active-link" : ""}`} onClick={closeMenuOnMobile}>
-              Collection
-            </Link>
-          </li>
-          <li>
-            <Link to="/about-us" className={`nav-link ${location.pathname === "/about-us" ? "active-link" : ""}`} onClick={closeMenuOnMobile}>
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className={`nav-link ${location.pathname === "/contact" ? "active-link" : ""}`} onClick={closeMenuOnMobile}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-        <div className="nav-close" id="nav-close" onClick={toggleMenu}><IoClose /></div>
-      </div>
+        <div className={`nav-menu ${showMenu ? "show-menu" : ""}`} id="nav-menu">
+          <ul>
+            <li>
+              <a href="#hero" className={`nav-link ${activeSection === "hero" ? "active-link" : ""}`} onClick={closeMenuOnMobile}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#t-shirts" className={`nav-link ${activeSection === "t-shirts" ? "active-link" : ""}`} onClick={closeMenuOnMobile}>
+                Merch
+              </a>
+            </li>
+            <li>
+              <a href="#about" className={`nav-link ${activeSection === "about" ? "active-link" : ""}`} onClick={closeMenuOnMobile}>
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className={`nav-link ${activeSection === "contact" ? "active-link" : ""}`} onClick={closeMenuOnMobile}>
+                Contact
+              </a>
+            </li>
+          </ul>
+          <div className="nav-close" id="nav-close" onClick={toggleMenu}><IoClose /></div>
+        </div>
 
-      <div className="nav-toggle" id="nav-toggle" onClick={toggleMenu}><IoMenu /></div>  
+        <div className="nav-toggle" id="nav-toggle" onClick={toggleMenu}><IoMenu /></div>  
       </nav>
     </header>
   );
